3e862d40c39539fa2f7cf218a0873c7138a6d29d
Ricard Illa <ricard@trkkn.com>
2023-05-19 17:17:34 +0200

    feat: added justfiles

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.gnumake pkgs.makefile2graph pkgs.graphviz ];
+          packages =
+            [ pkgs.gnumake pkgs.makefile2graph pkgs.graphviz pkgs.just ];
         };
       });
 }
